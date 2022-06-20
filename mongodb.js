47543582fd06841c89050c77361598c531e32c22
kevit <rdsankhavara2207@gmail.com>
2022-06-20 12:10:21 +0530

    CRUD in mongo

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+const mongodb = require("mongodb");
+
+// const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = mongodb;
+
+const url = "mongodb://127.0.0.1:27017";
+const database = "task-manager";
+
+// Creating id using ObjectID
+// const id = new ObjectID();
+// console.log(id.id.length);
+// console.log(id.toHexString().length);
+
+MongoClient.connect(url, (error, client) => {
+    if (error)
+        return console.log(error);
+
+    const db = client.db(database);
+})
